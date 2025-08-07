dd06a5877d18ffa4af3547319dc6c3bada957338
shiminhui <167271573+shiminhui@users.noreply.github.com>
2025-08-07 18:58:05 +0800

    Update CNV_detection.sh

--- CNV_detection.sh
+++ CNV_detection.sh
@@ -1,14 +1,16 @@
-==> step1.sh <==
-cnvnator -root AT01.root -tree AT01.marked.bam -chrom NC_056660.1 NC_056666.1 NC_056672.1 NC_056674.1
+#!/bin/bash
 
-==> step2.sh <==
+#step1
+cnvnator -root AT01.root -tree AT01.marked.bam -chrom chr1 chr2 chr3 chr4
+
+#step2
  cnvnator -root AT01.root -his 1000 -d dir/genome/
 
-==> step3.sh <==
+#step3
 cnvnator -root AT01.root -stat 1000
 
-==> step4.sh <==
+#step4
 cnvnator -root AT01.root -partition 1000
 
-==> step5.sh <==
+#step5
 cnvnator -root AT01.root -call 1000 > AT01.root.call.txt
